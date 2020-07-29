8b16c5a5477edfcd965f7bb352e5da2c30b550fd
johnsondaniel48 <johnsondaniel48@gmail.com>
2020-07-29 13:12:08 +1000

    Registartion and JsonWebtoken
    Added user info to the database.
    Generated JsonWebToken and returned it back.

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,16 +1,17 @@
 const { model } = require('mongoose');
 const {check,validationResult}=require('express-validator');
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt= require('jsonwebtoken');
 
-const router= require('express').Router();
-
-const user= require('../models/User');
 
 router.post('/',
 [
     check('name','Please provide a name').not().isEmpty(),
     check('email','Please provide email').isEmail(),
     check('password','Password must be 6 character long').isLength({min:6})
-], (req,res)=>{
+], async (req,res)=>{
    const validationMessage = validationResult(req);
     if(!validationMessage.isEmpty()){
     return res.status(400).json({errors:validationMessage.array()});
@@ -18,11 +19,37 @@ router.post('/',
     
    const {name,email,password}=req.body;
    try{
-       let user= await 
+       let user= await User.findOne({email});
+       if(user){
+           return res.status(400).json({msg:"user already exists"});
+       }
+
+       user = new User({
+           name,
+           email,
+           password
+       })
+
+       const salt= await bcrypt.genSalt(10);
+       user.password=await bcrypt.hash(password,salt);
+       await user.save();
+
+       const payload= {
+           user:{
+               id:user.id
+           }
+       }
+
+       jwt.sign(payload,process.env.secret,{expiresIn:3600},(err,token)=>{
+           if(err)
+           throw err;
+           res.send({token});
+       })
 
    }
    catch(err){
-
+    console.log(err.message);
+    res.status(500).send('server error')
    }
 })
 
