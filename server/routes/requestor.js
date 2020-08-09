fd832f58009f178730af4b46bf421190f3fba3e2
johnsondaniel48 <johnsondaniel48@gmail.com>
2020-08-10 07:47:40 +1000

    Removed sample code

--- server/routes/requestor.js
+++ server/routes/requestor.js
@@ -51,53 +51,7 @@ router.post('/',auth,
 })
 
 module.exports=router;
-
-
-
-
-    
-
-
-  
-  
-  
-    
-      
-      
-      
     
     
     
-      // get both selectors
-var dropdowns = document.querySelectorAll('.ac-register-interest-form select');
-// for every dropdown
-Array.from(dropdowns).forEach(function (dropdown, index) {
-    // watch for it to change
-    dropdown.addEventListener('change', (e) => {
-        // determine what the other selector is
-        // (the first one has ID 0, so the second one must have ID 1)
-        // (we just swap the number between 0 and 1 depending on what dropdown has been selected)
-        otherDropdownIndex = 1 - index;
-        // for every item within the OTHER dropdown
-        
-        for(var dropdownIter=0; dropdownIter<=2; dropdownIter++){
-           
-            if(index!=dropdownIter){
-              for (var dropdownValueIter = 0; dropdownValueIter < dropdowns[dropdownIter].length; dropdownValueIter++) {
-               // check to see if they're the same
-                if((dropdowns[dropdownIter][dropdownValueIter].value.toLowerCase() == dropdown.value.toLowerCase())|| dropdowns[dropdownIter][dropdownValueIter].value.toLowerCase() == dropdowns[dropdownIter].value)
-                {   // if they are the same, disable it!
-                dropdowns[dropdownIter][dropdownValueIter].disabled = true ;
-                }else{
-                    dropdowns[dropdownIter][dropdownValueIter].disabled = false; 
-                }
-             }
-           }
-        }
-    });
-});
-    
-    
-  
-  
 
